378ee4cd2cf33768e54564062634074a3f0bc05f
Dan Schultzer <dan@dreamconception.com>
2018-08-10 19:57:35 -0700

    Set phoenix and ecto as optional deps

--- mix.exs
+++ mix.exs
@@ -35,9 +35,9 @@ defmodule Pow.MixProject do
 
   defp deps do
     [
-      {:ecto, "~> 2.2.0"},
-      {:phoenix, "~> 1.3.0"},
-      {:phoenix_html, ">= 2.0.0 and <= 3.0.0"},
+      {:ecto, "~> 2.2.0", optional: true},
+      {:phoenix, "~> 1.3.0", optional: true},
+      {:phoenix_html, ">= 2.0.0 and <= 3.0.0", optional: true},
       {:plug, ">= 1.5.0 and < 1.7.0", optional: true},
 
       {:phoenix_ecto, "~> 3.3.0", only: [:dev, :test]},
